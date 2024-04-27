c3a64c8bdd1b9e36275e0d64ab61af824e1232ef
DenisAlexandrov <aie-dinis2007@yandex.ru>
2024-04-27 13:36:21 +0300

    Working out the text field and connecting the text field to the label

--- LoginApp/EntranceViewController.swift
+++ LoginApp/EntranceViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class EntranceViewController: UIViewController {
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
+    var userName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        let startColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.5).cgColor
+        let endColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 0.5).cgColor
+        
+        gradientLayer.colors = [startColor, endColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        if let userName = userName {
+            greetingLabel.text = "Привет, \(userName)!"
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
     }
-    */
-
 }
--- LoginApp/MainViewController.swift
+++ LoginApp/MainViewController.swift
@@ -7,27 +7,54 @@
 
 import UIKit
 
-final class MainViewController: UIViewController {
+class MainViewController: UIViewController {
     
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
+    @IBOutlet var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        passwordTextField.isSecureTextEntry = true
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Очищаем поля ввода
+        nameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EntranceViewController" {
+            if let destinationVC = segue.destination as? EntranceViewController {
+                destinationVC.userName = nameTextField.text
+            }
+        }
+    }
+    
     
     @IBAction func logInButton(_ sender: UIButton) {
-        let inputName = nameTextField.text ?? ""
-        let inputPassword = passwordTextField.text ?? ""
         
-//        if isValidName(enteredName) && isValidPassword(enteredPassword) {
-//            performSegue(withIdentifier: "loginSegue", sender: nil)
-//        }
+        let username = nameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if isValidCredentials(username: username, password: password) {
+            performSegue(withIdentifier: "EntranceViewController", sender: nil)
+        } else {
+            showErrorAlert()
+        }
     }
     
+
     @IBAction func UserNameAction(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Oops!", message: "Your name is Bond😎", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your name is Bond😎",
+            preferredStyle: .alert)
+        
         let completed = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(completed)
         
@@ -35,11 +62,37 @@ final class MainViewController: UIViewController {
     }
     
     @IBAction func UserPasswordAction(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Oops!", message: "Your password is 007🤘", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Oops!",
+            message: "Your password is 007🤘",
+            preferredStyle: .alert)
         let completed = UIAlertAction(title: "Ok", style: .default, handler: nil)
         alert.addAction(completed)
         
         present(alert, animated: true, completion: nil)
     }
+    
+    func isValidCredentials(username: String, password: String) -> Bool {
+        
+        let validUsername = "d"
+        let validPassword = "1"
+        
+        return username == validUsername && password == validPassword
+    }
+
+    func showErrorAlert() {
+        let alert = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter correct login and password",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.nameTextField.text = ""
+            self.passwordTextField.text = ""
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 }
+
+     
+
 
